5cb941f69221687b5cc31761db2f4f155cedb10a
aimnoii555 <aimnoii555@gmail.com>
2025-11-03 21:18:32 +0700

    refactor: use environment variables for database connection

--- config/connect.js
+++ config/connect.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize')
 
 
-const sequelize = new Sequelize('db_workshop_pos', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false,
 })
